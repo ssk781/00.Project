caf1482a935f70a0dd6dbd4a63db9aa00a96ba85
ssk781 <ssk781@miami.edu>
2025-03-21 11:00:50 -0400

    Update project to reflect current folder status

--- DSaSB_lab/03.bash/bam_to_fastq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#extract unmapped read 
-
-#samtools view -f 4 -b /Users/stephanie/Downloads/SK1.filtered_subreads.bam > /Users/stephanie/Downloads/SK1.unmapped.bam
-
-samtools fastq /Users/stephanie/Downloads/SK1.unmapped.bam > /Users/stephanie/Downloads/SK1.unmapped_reads.fastq
